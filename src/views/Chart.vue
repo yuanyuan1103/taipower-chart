a43ce8a23c2398974a27cf4f9ece8376455efa7b
yuanyuan1103 <pijhs70401@gmail.com>
2022-07-26 09:49:00 +0800

    new chart

--- src/views/Chart.vue
+++ src/views/Chart.vue
@@ -1,38 +1,145 @@
 <template>
-  <div />
+  <VueApexCharts
+    v-if="!isLoading"
+    type="line"
+    width="100%"
+    :options="chartOptions"
+    :series="series"
+  />
 </template>
 
 <script>
+import VueApexCharts from "vue3-apexcharts";
+
 export default {
   components: {
-    // LineChart,
+    VueApexCharts,
   },
   data() {
     return {
-      api: "https://script.google.com/macros/s/AKfycbxiI5h8LTu5EV6iOdZ5NtJsjzjKRa0Tq75gxNVaKkH2MmMX3KWt6ErXZJigxpUu5bMM7g/exec",
+      api: "https://script.google.com/macros/s/AKfycbwQUMHFzgMkQMOjJHDj9286__c_ViUqQQX3Xg2yctKJHyHC9yrh-TGjujQtOYA5bp5KCA/exec",
       isLoading: true,
-      chartdata: [],
       date: [],
       peak_loading: [],
       reserve_capacity: [],
       reserve_rate: [],
+
+      chartOptions: {
+        chart: {
+          height: 350,
+          zoom: { enabled: false },
+        },
+        title: {
+          text: "Taipower-chart",
+        },
+        tooltip: {
+          x: {
+            show: false,
+            // formatter: (val, opts) =>
+            //   opts.w.globals.lastXAxis.categories[opts.dataPointIndex],
+          },
+        },
+        xaxis: {
+          type: "category",
+          categories: ["2022/01/01", "2022/01/02", "2022/01/03"],
+          labels: {
+            formatter: (val) => {
+              const date = new Date(val);
+              return date.getDate() === 1
+                ? `${date.getFullYear()}年${date.getMonth() + 1}月`
+                : val;
+            },
+            rotate: -10,
+          },
+          // tooltip: {
+          //   formatter: function (val, opts) {
+          //     console.log(val);
+          //   },
+          // },
+        },
+        yaxis: [
+          {
+            seriesName: "瞬時尖峰負載",
+            //seriesName: "peak_loading",
+            max: 4800,
+            min: 0,
+            axisBorder: { show: true },
+            title: { text: "萬瓩" },
+            labels: { formatter: (val) => val },
+          },
+          {
+            seriesName: "瞬時尖峰負載",
+            // seriesName: "peak_loading",
+            show: false,
+          },
+          {
+            seriesName: "備轉容量率",
+            // seriesName: "reserve_rate",
+            opposite: true,
+            max: 30,
+            min: 0,
+            axisBorder: { show: true },
+            title: { text: "備轉率" },
+            labels: { formatter: (val) => `${val}%` },
+          },
+        ],
+      },
+      series: [
+        {
+          name: "瞬時尖峰負載",
+          // name: "peak_loading",
+          type: "line",
+          data: [2566.1, 2591.5, 2997.6],
+        },
+        {
+          name: "備轉容量",
+          // name: "reserve_capacity",
+          type: "column",
+          data: [334.5, 364.1, 318.4],
+        },
+        {
+          name: "備轉容量率",
+          // name: "reserve_rate",
+          type: "area",
+          data: [13.04, 14.05, 10.62],
+        },
+      ],
     };
   },
   methods: {
+    updateChart() {
+      //將日期傳給X軸
+      this.chartOptions = {
+        ...this.chartOptions,
+        xaxis: { ...this.chartOptions.xaxis, categories: this.date },
+      };
+
+      // console.log("config", {
+      //   ...this.chartOptions,
+      //   xaxis: { ...this.chartOptions.xaxis, categories: this.date },
+      // });
+
+      //將三種資料分別傳入Y軸
+      this.series = this.series.map((item) => ({
+        ...item,
+        data: this[item.name],
+      }));
+    },
     getTaipower() {
       this.$http
         .get(this.api)
         .then((resp) => {
           if (resp.data?.status !== "success") throw Error(resp.data?.message);
 
-          // this.chartdata = resp.data?.data;
-          let data = resp.data?.data;
-          // data = data.slice(0, 30);
+          const data = resp.data?.data;
 
           this.date = data.map((item) => item.date);
           this.peak_loading = data.map((item) => item.peak_loading);
-          this.reserve_capacity = data.map((item) => item.reserve_capacity);
-          this.reserve_rate = data.map((item) => item.reserve_rate);
+          this.瞬時尖峰負載 = data.map((item) => item.peak_loading);
+          this.備轉容量 = data.map((item) => item.reserve_capacity);
+          this.備轉容量率 = data.map((item) => item.reserve_rate);
+
+          this.updateChart();
 
           this.isLoading = false;
         })
@@ -43,6 +150,7 @@ export default {
   },
   mounted() {
     this.getTaipower();
+    this.isLoading = false;
   },
 };
 </script>
